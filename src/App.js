1ba56cd03aa1ebaa4d88fad13098b8135d766fad
Ayaryga <alekseyyaryga73@gmail.com>
2023-11-29 15:20:35 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [data, setData] = useState({});
+  const [town, setTown] = useState("");
+  const key = "5998041cc0cddb361e717eea5d0be21a";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${town}&units=metric&appid=${key}`;
+
+  const cearchWeather = (event) => {
+    if (event.key === "Enter") {
+      fetch(url)
+        .then((response) => response.json())
+        .then((response) => {
+          setData(response);
+        });
+      setTown("");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="inp-field">
+        <input
+          type="text"
+          value={town}
+          onChange={(event) => setTown(event.target.value)}
+          placeholder="Enter location"
+          onKeyDown={cearchWeather}
+        />
+      </div>
+      <div className="conteiner">
+        <div className="header">
+          <div className="city">
+            <p>{data.name}</p>
+          </div>
+        </div>
+        <div className="temp">
+          {data.main ? (
+            <h1>
+              {data.main.temp.toFixed()}
+              °C
+            </h1>
+          ) : null}
+        </div>
+        <div className="desc">
+          {data.weather ? <p>{data.weather[0].main}</p> : null}
+        </div>
+      </div>
+      {data.name !== undefined && (
+        <div className="footer">
+          <div className="fills">
+            {data.main ? (
+              <p className="bold">
+                {data.main.feels_like.toFixed()}
+                °C
+              </p>
+            ) : null}
+             <p>Відчувається як</p>
+          </div>
+          <div className="humidity">
+          {data.main ? (
+              <p className="bold">
+                {data.main.humidity}
+                %
+              </p>
+            ) : null}
+             <p>Вологість</p>
+          </div>
+          <div className="wind">
+          {data.main ? (
+              <p className="bold">
+                {`${data.wind.speed} `}
+                M/C
+              </p>
+            ) : null}
+             <p>Швидкість вітру</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
